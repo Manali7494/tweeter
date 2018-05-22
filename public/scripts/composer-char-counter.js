921f11613e6924f761f4471e7200c53cf2002df1
Manali Bhattacharyya <manalibhattacharyya10@gmail.com>
2018-05-22 21:06:58 +0000

    character count feature added

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,13 @@
 $(document).ready(function() {
-  console.log('working');
+  var length;
+  $('.new-tweet').on('keyup', 'textarea', function(event){
+    length = 140 - (this.textLength);
+    var spanCounter = this.parentElement.children[2];
+    $(spanCounter).text(length);
+    if (length < 0){
+      $(spanCounter).css("color", "red");
+    } else {
+      $(spanCounter).css("color", "black");
+    }
+  });
 });
